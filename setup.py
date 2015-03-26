2279984123c43b4c4fea68995071a0c67bba09d4
Anze Kolar <me@akolar.com>
2015-03-26 16:16:38 +0100

    Added requirements

--- setup.py
+++ setup.py
@@ -7,5 +7,12 @@ setup(name='Training planer',
       author_email='me@akolar.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=[
-          'Django>=1.7'],
-      )
+          'Django==1.7.3',
+          'Pint==0.6',
+          'fitparse==0.0.1-dev',
+          'django-jsonfield==0.9.13',
+          'django-bootstrap3==5.0.3',
+          'django-allauth==0.19.0',
+          'djorm-pgarray==1.2'
+      ],
+)
